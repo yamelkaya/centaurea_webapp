f25be74285b58bdfff4353a1ed56ff2cfc8c6dc2
yamelkaya <natalia.evsyuchenya@gmail.com>
2014-12-04 19:12:56 +0300

    master page for services

--- src/controllers/servicesController.js
+++ src/controllers/servicesController.js
@@ -8,18 +8,16 @@ ServicesController.prototype = {
 
     development: function(){
 
-        var file = fs.readFileSync('./public/services/development.html', 'utf-8');
-        template = hogan.compile(file);
+        var file = fs.readFileSync('./public/services/partials/development.html', 'utf-8');
+        var template = hogan.compile(file);
         var model = {
-            technologyName:"MongoDB",
-            bodyText: "can help with anything companies need related to MongoDB. Everything from the application code, prototype,"+
-            "design, and development, all the way to deployment of the application within your architecture." +
-            "Specifically our team can help clients with application design, schema design, deployment architecture,"+
-            "enterprise scaling, query optimization, benchmarking and much more."
+            partialHtml: template.render()
         };
 
-        return template.render(model);
+        var master = fs.readFileSync('./public/services/master.html','utf-8');
+        var masterTemplate = hogan.compile(master);
 
+        return masterTemplate.render(model);
     }
 
 };
--- src/controllers/technologiesController.js
+++ src/controllers/technologiesController.js
@@ -20,6 +20,22 @@ TechnologiesController.prototype = {
 
         return template.render(model);
 
+    },
+
+    couchdb: function(){
+
+        var file = fs.readFileSync('./public/technologies/view.html', 'utf-8');
+        template = hogan.compile(file);
+        var model = {
+            technologyName:"CouchDB",
+            bodyText: "can help with anything companies need related to CouchDB. Everything from the application code, prototype,"+
+            "design, and development, all the way to deployment of the application within your architecture." +
+            "Specifically our team can help clients with application design, schema design, deployment architecture,"+
+            "enterprise scaling, query optimization, benchmarking and much more."
+        };
+
+        return template.render(model);
+
     }
 
 };
