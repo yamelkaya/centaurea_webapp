e920b78b8f8def919508c70a56b8ba9397da6db4
yamelkaya <natalia.evsyuchenya@gmail.com>
2014-12-15 17:41:23 +0300

    base SEO completed

--- src/controllers/servicesController.js
+++ src/controllers/servicesController.js
@@ -5,27 +5,37 @@ var ServicesController = function (){
 };
 
 ServicesController.prototype = {
-    'development': function(){
+    'software-development': function(){
         return this._loadPageAndCompile(
             {
                 pagePath: './public/partials/services/development.html',
-                pageTitle: 'Centaurea - Software development'
+                pageTitle: 'Software Design, Software Development',
+                pageDescription: 'Centaureа team is dealing with systems of different levels and complexity for clients from lifestyle businesses ' +
+                'and start-ups to the enterprise level companies and global market leaders.',
+                pageKeywords: 'software development, application development, solution development'
             },this._masterPath);
     },
 
-    'consulting': function(){
+    'it-consulting': function(){
         return this._loadPageAndCompile(
             {
                 pagePath: './public/partials/services/consulting.html',
-                pageTitle: 'Centaurea - Technology consulting'
+                pageTitle: 'Database Consulting, Big Data Consulting, Cloud Consulting, Technology Consulting',
+                pageDescription: 'Centaurea is ready to share and help you in making use of different technologies and' +
+                ' building agile and scalable systems.',
+                pageKeywords: 'database consulting, big data consulting, cloud consulting, centaurea consulting, centaurea consulting services, ' +
+                'data consulting, data consulting services, database consulting, database consulting services, technology consulting'
             },this._masterPath);
     },
 
-    'corporate-trainings': function(){
+    'corporate-it-training': function(){
         return this._loadPageAndCompile(
             {
                 pagePath: './public/partials/services/corporate-trainings.html',
-                pageTitle: 'Centaurea - Corporate trainings'
+                pageTitle: 'Corporate IT Training',
+                pageDescription: 'Centaurea is ready to teach your team how to use the most modern technologies, share the best practices and go' +
+                'through the typical problems and development scenarios.',
+                pageKeywords: 'corporate training, it training'
             },this._masterPath);
     },
 
@@ -33,7 +43,9 @@ ServicesController.prototype = {
         return this._loadPageAndCompile(
             {
                 pagePath: './public/partials/services/cto-as-a-service.html',
-                pageTitle: 'Centaurea - CTO as a service'
+                pageTitle: 'Virtual CTO, CTO Services',
+                pageDescription: 'Centaurea is ready to offer you the best specialists fo the role of virtual CTO or VP of development.',
+                pageKeywords: 'CTO as a service, virtual CTO, virtual VP of development, CTO services'
             },this._masterPath);
     },
 
@@ -41,7 +53,10 @@ ServicesController.prototype = {
         return this._loadPageAndCompile(
             {
                 pagePath: './public/partials/services/dedicated-team.html',
-                pageTitle: 'Centaurea - Dedicated team'
+                pageTitle: 'Dedicated Team',
+                pageDescription: 'Centaureа is ready to offer you a team of highly qualified professionals with' +
+                ' necessary skills and deep knowledge needed to achieve the client`s goals while spending reasonable amount of resources.',
+                pageKeywords: 'dedicated team'
             },this._masterPath);
     },
 
@@ -49,7 +64,9 @@ ServicesController.prototype = {
         return this._loadPageAndCompile(
             {
                 pagePath: './public/partials/services/managed-services.html',
-                pageTitle: 'Centaurea - Managed Services'
+                pageTitle: 'Managed Services',
+                pageDescription: 'Stable infrastructure and safeness of data is an essential requirement for any business. Protect your data and keep your system healthy and well performed with our help.',
+                pageKeywords: 'managed services'
             },this._masterPath);
     },
 
@@ -57,7 +74,10 @@ ServicesController.prototype = {
         return this._loadPageAndCompile(
             {
                 pagePath: './public/partials/services/mongodb-as-a-service.html',
-                pageTitle: 'Centaurea - MongoDB as a service'
+                pageTitle: 'MongoDB as a Service',
+                pageDescription: 'Discover MongoDB which simply works. No problems. No backups. No scaling issues. For' +
+                ' any size of data and any performance requirements.',
+                pageKeywords: 'mongodb services, mongodb as a service'
             },this._masterPath);
     },
 
