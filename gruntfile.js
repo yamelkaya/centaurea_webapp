a792d44bb15032e4d4075ed7d286f4e2c09274c7
yamelkaya <natalia.evsyuchenya@gmail.com>
2014-12-25 22:39:04 +0300

    build package scripts prepared

--- gruntfile.js
+++ gruntfile.js
@@ -13,11 +13,85 @@ module.exports = function(grunt) {
                 useDotNotation: true,
                 consolidate: true
             }
+        },
+        clean: {
+            build: {
+                src: '../centaurea_build',
+                options: {
+                    force: true
+                }
+            }
+        },
+        copy: {
+            build: {
+                src: ['public/**/*','src/**/*','libs/**/*','node_modules/**/*','app.js','config.json'],
+                dest: '../centaurea_build/',
+                expand: true
+            }
+        },
+        uglify: {
+            build: {
+                options:{
+                    sourceMap: true
+                },
+                files: [{
+                    expand: true,
+                    src: '../cenaturea_build/**/*.js'
+                }]
+            }
+        },
+        cssmin: {
+            build: {
+                expand: true,
+                src: ['../centaurea_build/**/*.css', '!../centaurea_build/**/*.min.css'],
+                dest: '../centaurea',
+                ext: '.min.css'
+            }
+        },
+        htmlmin: {
+            build: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    src: [
+                        '../centaurea_build/**/*.html',
+                        '!../centaurea_build/public/partials/blog-post-preview/**/*.html',
+                        '!../centaurea_build/public/master/master-blog.html'
+                    ],
+                    dest: '../centaurea_build/'
+                }]
+            }
+        },
+        compress: {
+            build: {
+                options: {
+                    mode: 'gzip'
+                },
+                expand: true,
+                src: '../centaurea_build/public/**/*',
+                dest: '../centaurea_build/',
+                rename: function (src, dest){
+                    return dest+'.gz';
+                }
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-jasmine-node');
-    grunt.renameTask('jasmine_node', 'test');
-    grunt.registerTask('default', ['test']);
+    //grunt.loadNpmTasks('grunt-jasmine-node');
+    //grunt.renameTask('jasmine_node', 'test');
+
+    //
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+
+    grunt.registerTask('build',['clean','copy','uglify','cssmin','htmlmin','compress']);
+    grunt.registerTask('default', ['build']);
 };
